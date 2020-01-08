4ea3b8e6b11c5e9403ccad0fcf6cbcc9fb91404b
Sage Rimal <srimal@campusexplorer.com>
2020-01-07 21:03:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from flask import Flask, jsonify, request, abort
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
-
+#adding logger
 def _logger():
     '''
     Setup logger format, level, and handler.
